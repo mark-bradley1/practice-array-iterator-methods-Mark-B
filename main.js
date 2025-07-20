324bd8bd365febb770c94b322cfa425b8afc26fa
Mark Bradley <mbradle15@gmail.com>
2025-07-19 23:03:10 -0500

    Completed Practice Array Iterator Methods

---
+++ main.js
@@ -0,0 +1,23 @@
+//Task 1:
+let cities = ['Chicago', 'St. Louis', 'Phoenix', 'Kansas City', 'New York'];
+
+cities.forEach(city => {
+    console.log(city.toUpperCase());
+});
+
+//Task 2:
+let numbers = [1, 2, 3, 4, 5];
+let squares = numbers.map(num => num * num);
+console.log(squares); // Output: [1, 4, 9, 16, 25]
+
+//Task 3:
+let scores = [85, 42, 90, 75, 30, 100];
+let highScores = scores.filter(score => score >= 80);
+console.log(highScores); // Output: [85, 90, 100]
+
+//Task 4:
+let favoriteFood = ['Pizza', 'Tacos', 'Steak', 'Ice Cream', 'Cheeseburgers'];
+let firstFood = favoriteFood.find(food => food.length > 4);
+let firstFoodIndex = favoriteFood.findIndex(food => food.length > 4);
+console.log(firstFood);
+console.log(firstFoodIndex);
